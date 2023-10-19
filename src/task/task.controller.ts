6f081427d0f0ed6a9b2a5e8a0693a44a41656c5f
Denis Bortnikov <denis.bortnikov17@gmail.com>
2023-10-19 19:43:42 +0300

    add patch method

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Get } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
+  Param,
+} from "@nestjs/common";
 import { TaskService } from "./task.service";
+import { TaskDto } from "./task.dto";
 
 @Controller("tasks")
 export class TaskController {
@@ -7,11 +17,17 @@ export class TaskController {
 
   @Get()
   async getTasks() {
-    return [
-      {
-        name: "Record video",
-        isCompleted: false,
-      },
-    ];
+    return this.taskService.getAll();
+  }
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  async create(@Body() dto: TaskDto) {
+    return this.taskService.create(dto);
+  }
+
+  @Patch(":id")
+  async toggleDone(@Param("id") id: string) {
+    return this.taskService.toggleCompleted(id);
   }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { TaskDto } from "./task.dto";
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+  private TASKS = [
+    {
+      id: 1,
+      name: "Record video",
+      isCompleted: false,
+    },
+  ];
+
+  getAll() {
+    return this.TASKS;
+  }
+
+  create(dto: TaskDto) {
+    this.TASKS.push({
+      id: this.TASKS.length + 1,
+      name: dto.name,
+      isCompleted: false,
+    });
+
+    return this.TASKS;
+  }
+
+  toggleCompleted(id: string) {
+    const task = this.TASKS.find((task) => task.id === +id);
+    task.isCompleted = !task.isCompleted;
+    return task;
+  }
+}
